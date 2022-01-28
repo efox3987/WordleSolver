35d71511fe549594bd47fb4ef83246891c990af8
Ethan Fox <efox3987@gmail.com>
2022-01-28 16:12:46 -0500

    init

---
+++ genWordList.java
@@ -0,0 +1,43 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class genWordList {
+    public static void main(String[] args) throws Exception {
+        /**
+         * Creating the word list from 'words' unix file
+         */
+        ArrayList<String> wordList = new ArrayList<String>();
+        BufferedReader words;
+        try {words = new BufferedReader(new FileReader("/usr/share/dict/words"));
+            String word = words.readLine();
+            while(word != null) {
+                //Check if word is 5-letters
+                if(word.length() == 5) {
+                    if(!Character.isUpperCase(word.charAt(0))) {
+                        wordList.add(word);
+                    }
+                }
+                word = words.readLine();
+            }
+            words.close();
+        } catch (IOException e) {
+            System.out.println("Error finding words file.");
+            System.exit(1);
+        }
+
+        /**
+         * Generate new words file in this directory containning only 5 letter words
+         */
+        try {FileWriter newWords = new FileWriter("./fiveLetterWords.txt");
+            for(int i = 0; i < wordList.size(); i++) {
+                newWords.write(wordList.get(i) + "\n");
+            }
+            newWords.close();
+        } catch (IOException e) {
+            System.out.println("Error creating new words file");
+        }
+    }
+}
---
+++ wordleSolver.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class wordleSolver {
+    private static ArrayList<String> wordList;
+    private static Scanner keyboard;
+
+    private static void removeUnusableWords() {
+        System.out.println("Enter all letters that are unusable.");
+        System.out.print(">");
+        String letters = keyboard.nextLine();
+        char[] letterArr = letters.toCharArray();
+
+        for(int i = 0; i < letterArr.length; i++) {
+            for(int j = 0; j < wordList.size(); j++) {
+                if(wordList.get(j).contains(Character.toString(letterArr[i]))) {
+                    wordList.remove(j);
+                    j--;
+                }
+            }
+        }
+
+    }
+
+    private static void removeWordsWithout() {
+        System.out.println("Enter all the yellow characters.");
+        System.out.print(">");
+        String letters = keyboard.nextLine();
+        char[] letterArr = letters.toCharArray();
+
+        for(int i = 0; i < letterArr.length; i++) {
+            for(int j = 0; j < wordList.size(); j++) {
+                if(!wordList.get(j).contains(Character.toString(letterArr[i]))) {
+                    wordList.remove(j);
+                    j--;
+                }
+            }
+        }
+    }
+    private static void removeWordsWithoutGreen() {
+        System.out.println("How many green characters are there?");
+        System.out.print(">");
+        int numOfGreen = keyboard.nextInt();
+
+        for(int i = 0; i < numOfGreen; i++) {
+            System.out.println("Enter a green character.");
+            System.out.print(">");
+            String letters = keyboard.next();
+            char currChar = letters.charAt(0);
+            if(!Character.isLetter(currChar)) {
+                System.out.println("Enter a letter first!");
+                System.exit(1);
+            }
+
+            System.out.println("Enter its index.");
+            System.out.print(">");
+            int charIndex = keyboard.nextInt();
+            for(int j = 0; j < wordList.size(); j++) {
+                if(wordList.get(j).charAt(charIndex) != currChar) {
+                    wordList.remove(j);
+                    j--;
+                }
+            }
+        }
+        keyboard.close();
+    }
+    public static void main(String[] args) throws Exception {
+        wordList = new ArrayList<String>();
+        keyboard = new Scanner(System.in);
+
+        BufferedReader words;
+        try {
+            words = new BufferedReader(new FileReader("./fiveLetterWords.txt"));
+            String word = words.readLine();
+            while(word != null) {
+                wordList.add(word);
+                word = words.readLine();
+            } 
+        } catch (IOException e) {
+                System.out.println("Error making word list");
+        }
+        
+        removeUnusableWords();
+        removeWordsWithout();
+        removeWordsWithoutGreen();
+        keyboard.close();
+        System.out.println(wordList);
+    }
+}
